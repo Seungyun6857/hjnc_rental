451f08293630baa87120a4ecb265b6b4c730ad53
lsy6857 <kayato8703@gmail.com>
2025-10-14 15:02:19 +0900

    관리자 페이지 오류 수정 및 model_name 컬럼 추가

--- app.py
+++ app.py
@@ -1769,4 +1769,4 @@ ensure_tables()
 print("DB URL:", DATABASE_URL)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
